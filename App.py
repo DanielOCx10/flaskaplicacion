2d124041e52c33237fc0c6fa8c328214d777c9df
DanielOCx10 <danielortega.55p@gmail.com>
2026-05-06 22:24:00 -0700

    Guardar cambios en App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# jjejejeks
+# jjejejeks X modificacion.
 # MySQL Connection
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
